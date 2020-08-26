4efaba6e8d531112038180b9aab6b8bd7a562f52
MdAbuZehadAntu <tantu249@gmail.com>
2020-08-26 16:08:46 +0600

    new

---
+++ powerUsingEfficientRecusion.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int pow(int m,int n)
+{
+    if(n==0)
+        return 1;
+    if(n%2==0)
+    {
+        return pow(m*m,n/2);
+    }
+    else
+        return pow(m*m,n/2)*m;
+
+}
+
+int main()
+{
+    int m,n;
+
+
+        cin>>m>>n;
+        cout<<pow(m,n)<<endl;
+
+
+
+
+    return 0;
+}
+
